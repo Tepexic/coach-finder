82b9c487fe0380357c515a443ef8ef0b78aece80
Jesus Velazquez <jvelaz46@ford.com>
2020-09-23 12:15:28 -0500

    Add header and navigation bar component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
+  <TheHeader/>
   <router-view></router-view>
 </template>
 
+<script>
+  import TheHeader from './components/layout/TheHeader'
+
+  export default {
+    components: { TheHeader }  
+  }
+</script>
+
 <style>
-@import './assets/styles.css'
+  @import './assets/styles.css';
 </style>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,58 @@
+<template>
+  <header>
+    <nav class="md:flex md:justify-between md:items-center bg-gradient-to-r from-purple-700 to-blue-500 text-white shadow-lg">
+      <div class="flex items-center justify-between p-2">
+        <h1 class="text-2xl">
+          <router-link to="/">Find a coach</router-link>
+        </h1>
+        <button class="md:hidden rounded p-2 border-2 transition duration-500 ease-in-out transform hover:scale-110" @click="toggleMenu">
+          <svg class="w-6 fill-current md:hidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+      </div>
+      <div @click="toggleMenu">
+        <ul class="block text-center md:flex md:flex-row" :class="[ menu ? 'hidden' : ''  ]">
+          <li class="p-3 md:p-5">
+            <router-link to="/coaches">All Coaches</router-link>
+          </li>
+          <li class="p-3 md:p-5">
+            <router-link to="/requests">Requests</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+
+  data() {
+    return {
+      menu: false
+    }
+  },
+
+  methods: {
+    toggleMenu: function () {
+      return this.menu = !this.menu
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  @apply p-2;
+  @apply mx-4;
+  @apply rounded-sm;
+}
+
+a:active,
+a:hover,
+a.router-link-active {
+  border: 1px solid #fff;
+}
+</style>
